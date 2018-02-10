e7be5b98775d2a03938abced234f94f51ffe1e26
oleksandrstepanov <cbrzelviv@ukr.net>
2018-02-10 22:55:11 +0200

    Added ngif else statement

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = "testServer";
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -20,7 +21,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!'
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name server is'+ this.serverName;
   }
 
   onUpdateServerName(event: Event) {
